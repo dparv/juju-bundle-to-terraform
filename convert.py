5ded500eeccb0e79142acc39fb9ec6840909db4b
Diko Parvanov <diko.parvanov@canonical.com>
2024-01-18 02:20:03 +0200

    Added resource_offer to parse

--- convert.py
+++ convert.py
@@ -5,7 +5,19 @@ import sys
 f = open(sys.argv[1], 'r').read()
 juju_model_name = sys.argv[2]
 
-bundle = yaml.safe_load(f)
+docs = yaml.safe_load_all(f)
+
+num_docs = 0
+
+bundles = []
+for doc in docs:
+    num_docs+=1
+    bundles.append(doc)
+
+bundle = bundles[0]
+
+if num_docs == 2:
+    cmr_offers = bundles[1]
 
 resource_juju_model = """
 resource "juju_model" "{}" {{
@@ -54,17 +66,31 @@ resource "juju_integration" "{}" {{
 }}
 """
 
+resource_offer = """
+resource "juju_offer" "{}" {{
+  model            = juju_model.{}.name
+  application_name = juju_application.{}.name
+  endpoint         = {}
+}}
+"""
+
 print(resource_juju_model.format(juju_model_name, juju_model_name))
 
 if 'applications' in bundle:
     applications = bundle['applications']
-    #print(applications)
     application_names = list(applications.keys())
     for app_name in application_names:
         app = applications[app_name]
         charm = app['charm']
         channel = app['channel'] if 'channel' in app else ''
-        num_units = app['num_units'] if 'num_units' in app else 0
+        # num_units is part of baremetal bundles, scale - kubernetes, 0 means
+        # subordinate
+        if 'num_units' in app:
+            num_units = app['num_units']
+        elif 'scale' in app:
+            num_units = app['scale']
+        else:
+            num_units = 0
         const = app['constraints'] if 'constraints' in app else None
         charm_config = app['options'] if 'options' in app else None
         conf = ''
@@ -109,3 +135,20 @@ if 'relations' in bundle:
             right_app_name,
             right_app_relation
             ))
+
+
+if cmr_offers:
+    for app_name in application_names:
+        if app_name in list(cmr_offers['applications'].keys()):
+            if 'offers' in cmr_offers['applications'][app_name]:
+                offers = cmr_offers['applications'][app_name]['offers']
+                for offer in offers:
+                    endpoints = (offers[offer]['endpoints'])
+                    for endpoint in endpoints:
+
+                        print(resource_offer.format(
+                            offer,
+                            juju_model_name,
+                            app_name,
+                            endpoint
+                            ))
